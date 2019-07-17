6a118742ac45853f554f1d2a29c583e4d6fd3c06
kmo004 <kmo004@ucr.edu>
2019-07-17 14:25:29 -0700

    Add files via upload

--- turnin/kmo004_lab11_part1.c
+++ turnin/kmo004_lab11_part1.c
@@ -13,6 +13,51 @@
 #include "simAVRHeader.h"
 #endif
 
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+unsigned char button = 0x00;
+
+void TimerOn()
+{
+  TCCR1B = 0x0B;
+  OCR1A = 125;
+  TIMSK1 = 0x02;
+  TCNT1 = 0;
+
+  _avr_timer_cntcurr = _avr_timer_M;
+  
+  SREG |= 0x80;
+}
+
+void TimerOff()
+{
+  TCCR1B = 0x00;
+}
+
+void TimerISR()
+{
+  TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  _avr_timer_cntcurr--;
+  if (_avr_timer_cntcurr == 0)
+    {
+      TimerISR();
+      _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M)
+{
+  _avr_timer_M = M;
+  _avr_timer_cntcurr = _avr_timer_M;
+}
+
 unsigned char GetKeypadKed(){
 	PORTC = 0xEF;
 	asm("nop");
@@ -55,6 +100,7 @@ int main(void) {
     /* Insert your solution below */
     while (1) {
 		x = GetKeypadKed();
+		
 		switch(x) {
 			case '\0': PORTB = 0x1F; break;
 			case '1': PORTB = 0x01; break;
